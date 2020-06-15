7702cb45ceade9e8c49bed7ec18e5e0a33c13d6c
Joshua Kitterman <joshkitterman@gmail.com>
2020-06-15 10:28:28 -0600

    working but ugly

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
   assume_role {
-    role_arn = ""
+    role_arn = "arn:aws:iam::152204665473:role/cp-aws-sandbox-power-user-role"
   }
 }
--- sns.tf
+++ sns.tf
@@ -5,6 +5,7 @@ variable "testing" {
 variable "subscribers" {
   type = list(object({
     name = string
+    topics = list(string)
     accounts = list(string)
     topics = list(string)
   }))
@@ -12,42 +13,67 @@ variable "subscribers" {
     {
       name = "subscriber1"
       accounts = [
+        "166445975460", "1234"
       ]
       topics = [
-        "Sandbox-Topic"
+        "Sandbox-Topic", "Another Topic"
       ]
     },
     {
       name = "subscriber2"
       accounts = [
+        "838401776392", "5678"
       ]
       topics = [
-        "Sandbox-Topic"
+        "Sandbox-Topic2"
       ]
-    }
+    },
+    {
+      name = "subscriber3"
+      accounts = [
+        "166445975460", "9012"
+      ]
+      topics = [
+        "Sandbox-Topic", "Another Topic"
+      ]
+    },
   ]
 }
 
 locals {
   test = var.subscribers
-  a = flatten([
-  for s in var.subscribers : [
-  for a in s.accounts : [
-    a
-  ]
-  ]
-  ])
+  a = distinct(flatten([
+    for s in var.subscribers : [
+      for a in s.topics : s.accounts if a == "Another Topic"
+//      contains(s.topics, "Another Topic") ? s.accounts : ""
+    ]
+  ]))
 }
+
+//locals {
+//  test = var.subscribers
+//  a = flatten([
+//  for s in var.subscribers : [
+//  for a in s.topics : s.accounts if a == "Another Topic"
+//  //      contains(s.topics, "Another Topic") ? s.accounts : ""
+//  ]
+//  ])
+//}
 
 output "test" {
   value = local.a
 }
 
 resource "aws_sns_topic" "this" {
-  name = "Sandbox-Topic1"
+  name = "Sandbox-MyNewTopic"
 }
 
-data "aws_iam_policy_document" "sns_topic_policy" {
+resource "aws_sns_topic_policy" "this" {
+  arn = aws_sns_topic.this.arn
+  policy = data.aws_iam_policy_document.this.json
+}
+
+data "aws_iam_policy_document" "this" {
   policy_id = "__default_policy_ID"
   statement {
     sid = "__default_statement_ID"
@@ -89,13 +115,12 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     ]
     principals {
       type = "AWS"
-      identifiers = flatten([
-      for s in var.subscribers : [
-      for a in s.accounts : [
-        a
-      ]
-      ]
-      ])
+      identifiers = distinct(flatten([
+        for s in var.subscribers : [
+//          for a in s.topics : s.accounts if a == "Another Topic"
+            contains(s.topics, "Another Topic") ? s.accounts : []
+        ]
+      ]))
     }
     resources = [
       aws_sns_topic.this.arn
